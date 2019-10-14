78ecc5cf6797695cb093e9c39734f315c8a4d487
David Soards <davidysoards@gmail.com>
2019-10-14 14:29:33 -0400

    add lambda

---
+++ functions/hello.js
@@ -0,0 +1,44 @@
+const { zipFunctions } = require("@netlify/zip-it-and-ship-it");
+
+zipFunctions("functions", "functions-dist");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://vue-hq-66b28.firebaseio.com"
+});
+
+exports.handler = function(event, context, callback) {
+  const firestore = admin.firestore();
+
+  firestore
+    .collection("users")
+    .where("email", "==", "davidysoards@gmail.com")
+    .limit(1)
+    .get()
+    .then(response => {
+      if (response.empty) {
+        return Promise.reject();
+      }
+
+      const userInfo = response.docs[0].data();
+
+      console.log(userInfo);
+
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(userInfo)
+      });
+    })
+    .catch(error => {
+      console.log(error);
+
+      callback(null, {
+        statusCode: 500,
+        body: error
+      });
+    });
+};
